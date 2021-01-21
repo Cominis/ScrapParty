77fe73ab9d4d84ad527f777260097a3e067c6a94
Daumantas Šimkus <daumantas.simkus@gmail.com>
2021-01-21 11:37:56 +0200

    Add error handling

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var missingPartyButton = document.getElementById("missingPartyButton");
+  var button = document.getElementById("missingPartyButton");
 
-  missingPartyButton.addEventListener("click", () =>
+  button.addEventListener("click", () =>
     chrome.tabs.query({ active: true }, (tab) => activeTabCallback(tab))
   );
 });
 
 let activeTabCallback = (tab) => {
   findParticipants((participants) => {
-    window.open(
-      "https://www.google.com/search?q=" + JSON.stringify(participants),
-      "blank"
-    );
+    const list = document.getElementById("missingPartyList");
+    list.innerHTML = "";
+
+    const msg = document.getElementById("errorMessage");
+    msg.style.display = "none";
+
+    if (
+      typeof participants == "undefined" ||
+      participants == null ||
+      participants.length == 0
+    ) {
+      msg.style.display = "block";
+    } else {
+      for (let i = 0; i < participants.length; i++) {
+        const item = document.createElement("li");
+        item.appendChild(document.createTextNode(participants[i]));
+        list.appendChild(item);
+      }
+    }
   });
 };
 
