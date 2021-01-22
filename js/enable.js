83c92ff3bd08644561037312f0837ac225a99170
Daumantas Šimkus <daumantas.simkus@gmail.com>
2021-01-22 14:16:39 +0200

    Fix icon sizes

--- js/enable.js
+++ js/enable.js
@@ -4,17 +4,9 @@ let checkForValidUrl = (tabId, changeInfo, tab) => {
       chrome.pageAction.show(tabId);
     } else {
       chrome.pageAction.hide(tabId);
-      chrome.pageAction.setIcon({
-        tabId: tabId,
-        path: "images/logo.png",
-      });
     }
   } else {
     chrome.pageAction.hide(tabId);
-    chrome.pageAction.setIcon({
-      tabId: tabId,
-      path: "images/logo.png",
-    });
   }
 };
 
